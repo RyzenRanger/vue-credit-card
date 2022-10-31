d30692d20c50de2df673464da5f85946f667236f
RyzenRanger <tomreddler@gmail.com>
2022-10-31 23:53:21 +0300

    Installed vue-fliper

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,41 @@
 <template>
   <div id = "app">
+    <video autoplay muted loop class = "video-background">
+      <source src="@/assets/calling20221009.mp4" type="video/mp4">
+    </video>
+
+    <credit-card />
   </div>
 </template>
 
 <script>
+import CreditCard from '@/components/CreditCard.vue'
+
 export default {
   name: 'App',
+
+  components: { CreditCard },
 }
 </script>
 
 <style lang = 'scss'>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+}
+
+.video-background {
+  position: fixed;
+  top: 0;
+  left: 0;
 }
 </style>
---
+++ src/components/CreditCard.vue
@@ -0,0 +1,34 @@
+<template>
+	<vue-flip
+		:active-hover = "true"
+		width					= "400px"
+		height				= "250px"
+	>
+		<template v-slot:front>
+			true
+		</template>
+		<template v-slot:back >
+			false
+		</template>
+	</vue-flip>
+</template>
+
+<script>
+import VueFlip from 'vue-flip';
+
+export default {
+	name: 'credit-card',
+
+	components: { 'vue-flip': VueFlip },
+}
+</script>
+
+<style lang = 'scss'>
+.front {
+	background-color: red;
+}
+
+.back {
+	background-color: white;
+}
+</style>
